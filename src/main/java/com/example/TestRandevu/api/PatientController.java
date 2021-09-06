2eaec37e9e67cdfa6293050f30c732510da942d7
anghielo <user_noreply@github.com>
2021-09-06 01:43:23 -0700

    Create PatientController.java

---
+++ src/main/java/com/example/TestRandevu/api/PatientController.java
@@ -0,0 +1,30 @@
+package com.example.TestRandevu.api;
+
+import com.example.TestRandevu.dao.PatientService;
+import com.example.TestRandevu.model.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class PatientController {
+    @Autowired
+    PatientService patientService;
+    @GetMapping("/getPatientDetails")
+    public Patient getPatient(@RequestParam String name ) throws InterruptedException, ExecutionException {
+        return patientService.getPatientDetails(name);
+    }
+    @PostMapping("/createPatient")
+    public String createPatient(@RequestBody Patient patient ) throws InterruptedException, ExecutionException {
+        return patientService.savePatientDetails(patient);
+    }
+    @PutMapping("/updatePatient")
+    public String updatePatient(@RequestBody Patient patient ) throws InterruptedException, ExecutionException {
+        return patientService.updatePatientDetails(patient);
+    }
+    @DeleteMapping("/deletePatient")
+    public String deletePatient(@RequestParam String name){
+        return patientService.deletePatient(name);
+    }
+}
