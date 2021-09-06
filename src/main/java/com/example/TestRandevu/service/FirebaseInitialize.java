dc4f0dafb292aa7374d6f40f01d7760e584b1c3c
anghielo <user_noreply@github.com>
2021-09-06 01:16:32 -0700

    update

---
+++ src/main/java/com/example/TestRandevu/service/FirebaseInitialize.java
@@ -0,0 +1,27 @@
+package com.example.TestRandevu.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+
+@Service
+public class FirebaseInitialize {
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("./randevu-db-firebase-adminsdk-m0ngz-593727589d.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://chatapp-e6e15.firebaseio.com")
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
